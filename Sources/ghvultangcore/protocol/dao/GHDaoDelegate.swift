daebdadcb9795c6c99789e3d741d264d73092d65
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2022-08-08 17:42:21 -0500

    Se hace adecuacion sobre el dao no rx

--- Sources/ghvultangcore/protocol/dao/GHDaoDelegate.swift
+++ Sources/ghvultangcore/protocol/dao/GHDaoDelegate.swift
@@ -9,44 +9,15 @@ import ghgungnircore
 
 public protocol GHDaoDelegate {
     associatedtype T
+    associatedtype U
     
-    func get<U>(id: U) throws -> T?
+    func get(id: U) throws -> T?
     func getAll(closure: @escaping ([T]) -> ()) throws
     
     func save(model: T, closure: @escaping (Bool) -> ()) throws
     func saveAll(list: [T], closure clousure: @escaping (Bool) -> ()) throws
     
-    func delete<U>(id: U) throws
+    func delete(id: U) throws
     
     func deleteAll() throws
-}
-
-public extension GHDaoDelegate {
-    func get<U>(id: U) throws -> T? {
-        throw GHError.make(message: "get not implemented")
-    }
-    
-    func getAll(closure: @escaping ([T]) -> ()) throws {
-        throw GHError.make(message: "getAll not implemented")
-    }
-    
-    func save(model: T, closure: @escaping (Bool) -> ()) throws {
-        throw GHError.make(message: "save not implemented")
-    }
-    
-    func saveAll(list: [T]) throws {
-        throw GHError.make(message: "saveAll not implemented")
-    }
-    
-    func update(model: T) throws {
-        throw GHError.make(message: "update not implemented")
-    }
-    
-    func delete<U>(id: U) throws {
-        throw GHError.make(message: "delete not implemented")
-    }
-    
-    func deleteAll() throws {
-        throw GHError.make(message: "deleteAll not implemented")
-    }
 }
